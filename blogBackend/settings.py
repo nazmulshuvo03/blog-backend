d0fa195084bf5f73895c93e66f88469e4015f23e
Nazmul Alom <nazmulshuvo03@gmail.com>
2021-06-12 22:15:56 +0600

    db added

--- blogBackend/settings.py
+++ blogBackend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -46,7 +47,7 @@ INSTALLED_APPS = [
     'djoser',
     'ckeditor',
     'ckeditor_uploader',
-    'django_extensions'
+    'django_extensions',
 ]
 
 CKEDITOR_UPLOAD_PATH = "images/"
@@ -183,3 +184,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
